16d6e30ecab2cc395c7e746295f4c68a1aa6fbd6
Elladur <73045128+Elladur@users.noreply.github.com>
2023-10-19 22:03:16 +0200

    initial version

---
+++ matchscheduler/round.py
@@ -0,0 +1,116 @@
+"""A round of a season consisting of a list of matches."""
+from datetime import date
+
+from .match import Match, MatchFactory, NotValidMatch
+from .player import Player
+
+
+class Round:
+    """A round of a season consisting of a list of matches."""
+
+    def __init__(self, matches: list[Match], date_of_round: date, num_matches: int):
+        self.matches = matches
+        self.num_matches = num_matches
+        self.date = date_of_round
+
+    def is_valid(self) -> bool:
+        """Check if this round is valid."""
+        if len(self.matches) != self.num_matches:
+            return False
+        # every player can only play in one match,
+        # it should return false if any player is in more than one match
+        players_in_this_round = []
+        for match in self.matches:
+            for player in match.players:
+                if player in players_in_this_round:
+                    return False
+                players_in_this_round.append(player)
+
+        # every match has to have the same date as the round
+        for match in self.matches:
+            if match.date != self.date:
+                return False
+        return True
+
+    def swap_players(self, match_index: int, players: set[Player]) -> None:
+        """Swap the players in the given match with the given players."""
+        new_match = None
+        current_match = self.matches[match_index]
+        try:
+            new_match = Match(players, self.matches[match_index].date)
+            self.matches[match_index] = new_match
+        except NotValidMatch as exc:
+            raise NotValidSwap(
+                "Swap is not valid, because Match is not valid."
+            ) from exc
+        if not self.is_valid():
+            self.matches[match_index] = current_match
+            raise NotValidSwap("Swap is not valid.")
+
+    def swap_players_of_existing_matches(
+        self, match_index1: int, match_index2: int, players_to_swap: set[Player]
+    ) -> None:
+        """Swap the players in the given matches."""
+        new_match1 = None
+        new_match2 = None
+        current_match1 = self.matches[match_index1]
+        current_match2 = self.matches[match_index2]
+        try:
+            new_match1 = Match(
+                current_match1.players.symmetric_difference(players_to_swap), self.date
+            )
+            new_match2 = Match(
+                current_match2.players.symmetric_difference(players_to_swap), self.date
+            )
+            self.matches[match_index1] = new_match1
+            self.matches[match_index2] = new_match2
+        except NotValidMatch as exc:
+            raise NotValidSwap(
+                "Swap is not valid, because Match is not valid."
+            ) from exc
+        if not self.is_valid():
+            self.matches[match_index1] = current_match1
+            self.matches[match_index2] = current_match2
+            raise NotValidSwap("Swap is not valid.")
+
+    def export_match_string(self) -> str:
+        """Export the matches of this round as a string."""
+        return "\n".join([str(match) for match in self.matches])
+
+    def get_players(self) -> set[Player]:
+        """Get the players in this round."""
+        players: set[Player] = set()
+        for match in self.matches:
+            players.update(match.players)
+        return players
+
+
+class NotValidRound(Exception):
+    """Raised when a round is not valid."""
+
+
+class NotValidSwap(Exception):
+    """Raised when a swap is not valid."""
+
+
+class RoundFactory:
+    """A factory for generating rounds."""
+
+    @staticmethod
+    def generate_valid_round(
+        players: set[Player], date_of_play: date, num_courts: int
+    ) -> Round:
+        """Generate a valid round for the given players and date."""
+        matches: list[Match] = []
+        for i in range(num_courts):
+            for match in MatchFactory.generate_valid_new_match(
+                players, date_of_play, matches
+            ):
+                new_proposed_round = Round(matches + [match], date_of_play, i + 1)
+                if new_proposed_round.is_valid():
+                    matches.append(match)
+
+        r = Round(matches, date_of_play, num_courts)
+        if r.is_valid():
+            return r
+        raise NotValidRound("Could not generate a valid round.")
---
+++ tests/test_player.py
@@ -0,0 +1,41 @@
+from datetime import date
+from matchscheduler.player import Player
+import pytest
+
+
+@pytest.mark.parametrize(
+    "name, date_input", [("John", "2021-01-01"), ("Jane", "2021-01-02")]
+)
+def test_player(name, date_input):
+    player = Player(name, [date_input])
+    assert player.name == name
+    assert player.cannot_play == [date.fromisoformat(date_input)]
+
+
+@pytest.mark.parametrize(
+    "dict_player",
+    [
+        {"name": "John", "cannot_play": ["2021-01-01", "2021-01-02"]},
+        {"name": "Jane", "cannot_play": ["2021-01-02"]},
+    ],
+)
+def test_from_dict(dict_player):
+    player = Player.from_dict(dict_player)
+    assert player.name == dict_player["name"]
+    assert player.cannot_play == [
+        date.fromisoformat(i) for i in dict_player["cannot_play"]
+    ]
+
+
+def test_set_to_tuple_works():
+    player1 = Player("John", ["2021-01-01"])
+    player2 = Player("Jane", ["2021-01-02"])
+    result = Player.set_to_tuple({player1, player2})
+    for player in result:
+        assert player in {player1, player2}
+
+
+def test_set_to_tuple_raises_error_if_not_two_players():
+    player1 = Player("John", ["2021-01-01"])
+    with pytest.raises(ValueError):
+        Player.set_to_tuple({player1})
