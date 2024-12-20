9405a383fae13c88015ba7faebbd5cb6372c0f7a
Elladur <mail@patrickreiner.at>
2024-12-20 17:08:29 +0000

    fix file

--- tests/unit/test_match.py
+++ tests/unit/test_match.py
@@ -93,38 +93,3 @@ def test_can_match_be_added(max, moritz, laura, clara):
     assert not can_match_be_added(matches, Match(laura, moritz))
     assert not can_match_be_added(matches, Match(max, None))
 
-#@pytest.mark.parametrize(
-    #"arg_match, expected", [((1, 1), [1, 1]), ((2, 3), [2, 3]), ((1, None), [1])]
-#)
-#def test_get_players_of_match(arg_match, expected):
-    #assert expected == match.get_players_of_match(arg_match)
-
-
-#@pytest.mark.parametrize(
-    #"arg_match, players",
-    #[
-        #((0, 1), [player.Player("Max", []), player.Player("Moritz", [])]),
-        #((0, None), [player.Player("Max", []), player.Player("Moritz", [])]),
-    #],
-#)
-#def test_convert_match_to_string(arg_match, players):
-    #result = match.convert_match_to_string(arg_match, players)
-    #assert result.find(players[arg_match[0]].name) != -1
-    #if arg_match[1] is not None:
-        #assert result.find(players[arg_match[1]].name) != -1
-    #else:
-        #assert result.find(players[1].name) == -1
-
-
-#def test_replace_player_in_match_works_properly():
-    #m = match.create_match(1, 2)
-    #new_match, swapped = match.replace_player_in_match(m, 2, 3)
-    #assert new_match == match.create_match(1, 3)
-    #assert swapped
-
-
-#def test_replace_player_in_match_doesnt_change_if_player_not_in_match():
-    #m = match.create_match(1, 2)
-    #new_match, swapped = match.replace_player_in_match(m, 3, 4)
-    #assert new_match == m
-    #assert not swapped
